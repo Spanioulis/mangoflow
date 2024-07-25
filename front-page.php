85058be2d5716f2aefb5071be81246ddbb01970c
Spanioulis <pani77sps@gmail.com>
2024-07-25 22:15:21 +0200

    Add mango logo with bottom/right absolute position and anchor to email on home page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,19 @@
 <?php get_header(); ?>
 
-<main class="template-home js-2cp">
-    <div class="export-index"></div>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit totam libero. Nesciunt rem impedit voluptatem tenetur amet! Animi, minus repellat recusandae aliquid doloremque a dicta quos quasi qui cumque?
-    Voluptatem magnam, minus omnis ea, exercitationem quasi quis commodi illo cumque porro id voluptas expedita distinctio eius corporis odit et illum minima officiis dolore officia a voluptatum ab! Ratione, perferendis.
-    Temporibus quae debitis asperiores ducimus numquam laudantium dicta quos voluptatum tempore, in, itaque harum laboriosam error inventore voluptate accusantium saepe dolorum non necessitatibus nulla? Aliquam officiis officia est possimus quam.</p>
-    <?php the_content(''); ?>
+<main class="template-home">
+    <h3><?php the_content(''); ?></h3>
+    <section class="s-cover js-1cp">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit totam libero. Nesciunt rem impedit voluptatem tenetur amet! Animi, minus repellat recusandae aliquid doloremque a dicta quos quasi qui cumque?
+        Voluptatem magnam, minus omnis ea, exercitationem quasi quis commodi illo cumque porro id voluptas expedita distinctio eius corporis odit et illum minima officiis dolore officia a voluptatum ab! Ratione, perferendis.
+        Temporibus quae debitis asperiores ducimus numquam laudantium dicta quos voluptatum tempore, in, itaque harum laboriosam error inventore voluptate accusantium saepe dolorum non necessitatibus nulla? Aliquam officiis officia est possimus quam.</p>
+    </section>
+
+    <!-- Logo -->
+    <figure>
+        <a href="mailto:valeriagereda@hotmail.com" target="_blank" rel="noopener noreferrer">
+            <img src="<?php echo get_template_directory_uri(  ); ?>/assets/img/mangoflow.png" alt="Mangoflow Studio logo">  
+        </a>
+    </figure>
 </main>
 
 <?php get_footer(); ?>
