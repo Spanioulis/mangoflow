b237cfd43739263f40b8fa52979fe9553221ecff
Spanioulis <pani77sps@gmail.com>
2024-09-26 15:57:53 +0200

    Update header styles

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <header id="header" class="header" role="banner">
     <a class="header__name" id="logo" href="<?= get_home_url(); ?>"><h3>Valeria Gereda</h3></a>
-    <a class="header__logo" id="logo" href="<?= get_home_url(); ?>"><p>mangoflow estudio</p></a>
+    <a class="header__logo" id="logo" href="<?= get_home_url(); ?>">mangoflow estudio</a>
     <!-- Páginas (menú) -->
     <nav class="mainmenu" id="main-menu">
         <?= wp_nav_menu(['theme_location'=> 'header-menu', 'container' => false, 'menu_class' => 'main-menu']); ?>
