c7c9e2b8292bcb06e91294b6f4c2c574fc96e58d
Spanioulis <pani77sps@gmail.com>
2024-11-10 14:19:51 +0100

    Add and deploy top 0 to footer

--- footer.php
+++ footer.php
@@ -1,10 +1,23 @@
         <footer class="footer">
-            <a href="<?= get_home_url(); ?>">
+            <a href="#top" class="footer__logo">
                 <h2>mangoflow estudio</h2>
             </a>
         </footer>
 
         <?php wp_footer(); ?>
 
+        <script>
+            document.addEventListener('DOMContentLoaded', () => {
+                const topButton = document.querySelector('.footer__logo');
+
+                topButton.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                });
+            });
+        </script>
     </body>
 </html>
