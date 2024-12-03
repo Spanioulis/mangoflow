c60d75f588175c300a3ce43ee313857fa6e40699
Spanioulis <pani77sps@gmail.com>
2024-12-03 10:44:25 +0100

    Create page-privacy

---
+++ page-privacy.php
@@ -0,0 +1,12 @@
+<?php
+/*Template Name: Página de "Política de Privacidad" */
+get_header(); ?>
+
+<main class="template-page-privacy">
+    <h1><?= get_the_title(); ?></h1>
+    <?= get_the_content(); ?>
+</main> 
+
+<?php get_footer(); ?>
+
+  
