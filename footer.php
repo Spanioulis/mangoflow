1ab33d117f4f9d9769d1ec07857cc27a3e365bae
Spanioulis <pani77sps@gmail.com>
2024-12-04 11:26:04 +0100

    Add social icons

--- footer.php
+++ footer.php
@@ -1,4 +1,28 @@
         <footer class="footer">
+            <ul>
+                <li>
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                        <svg>
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/social.svg#instagram"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                        <svg>
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/social.svg#linkedin"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="phone:+5692350901" target="_blank" rel="noopener noreferrer">
+                        <svg>
+                            <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/icons/social.svg#whatsapp"></use>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+
             <a href="#top" class="footer__logo">
                 <h2>mangoflow estudio</h2>
             </a>
